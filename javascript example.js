15c2f046c38e17e2973e2ebea38cfd27e6bc97ab
KnightFalkon <fro14006@byui.edu>
2018-02-10 13:27:11 -0700

    Adding teaching example

---
+++ javascript example.js
@@ -0,0 +1,40 @@
+var isTrue = true;
+var num = 0;
+
+// while(isTrue) {
+// 	console.log(num);
+// 	num += 1;
+// 	if(num > 5) {
+// 		isTrue = false;
+// 	}
+// }
+
+function doStuff(a, b, c) {
+	console.log( a );
+	console.log(b);
+	console.log(c);
+	// for(var i = 0; i < 5; ++i) {
+	// 	console.log(i);
+	// }
+}
+
+function example() {
+	var a = "stuff";
+	var b = 2;
+	var c = false;
+
+	doStuff(a,b, c);
+	
+}
+
+example();
+
+// if(false) {
+// 	// do the code that's in here
+// } else if (true) {
+// 	//do stuff
+// } else if (true) {
+
+// } else {
+// 	// do stuff
+// }
