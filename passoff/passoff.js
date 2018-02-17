54d778a7497a41f2ab1fc0f6112fe5e869bea834
KnightFalkon <fro14006@byui.edu>
2018-02-17 10:18:03 -0700

    added some things and added a json file

--- passoff/passoff.js
+++ passoff/passoff.js
@@ -1,17 +1,17 @@
 //This is the data, usually it would be stored on a server
-var jsonString = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+// var jsonString = '{ "employees" : [' +
+// '{ "firstName":"John" , "lastName":"Doe" },' +
+// '{ "firstName":"Anna" , "lastName":"Smith" },' +
+// '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 //This turns the json into a usable js object
-var obj = JSON.parse(jsonString);
+var obj;
 
 //This is used so that the first time the user clicks the button it will always tell them to try again
 var first = true;
 
 //This adds the printPeople method to obj
 //It will add the informaiton from obj into a text string and then display it.
-obj.printPeople = function(count) {
+function printPeople() {
     var text = "";
     //This gives a random number between 0-99
     var ranNum = Math.floor(Math.random() * 100);
@@ -27,20 +27,36 @@ obj.printPeople = function(count) {
 //This displays the information in obj or will print an error message depending on the ranNum
 function display(text, ranNum) {
     var mod = ranNum % 2;
-    errText = "Not this time, try agian!";
-    var textArea = document.getElementById("people");
-
+    var errText = "Not this time, try agian!";
+    var output = "";
     //if ranNum is odd, it will print an error message
     if(mod != 0 || first) {
-        textArea.value = errText;
+        output = errText
         first = false;
     } //If it is even it will print the information from obj
     else {
-        textArea.value = text;    
+        output = text;    
     }
+
+    var p = document.createElement('P');
+    var t = document.createTextNode(output);
+    p.appendChild(t);
+    document.getElementById("peoplediv").appendChild(p);
 }
+
+function loadJSON() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        obj = this.responseText;
+      }
+    };
+    xhttp.open("GET", "./passoff.json", true);
+    xhttp.send();
+  }
 
 //main driver function
 function doStuff() {
-    obj.printPeople();
+    loadJSON();
+    printPeople();
 }
